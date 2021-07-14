78f3dfa66008098fc11efd519526830c961f89c8
Alejandro Lopez Stanley <lexls@icloud.com>
2021-07-14 15:05:52 -0300

    Init

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() { Id=1, Nombre="Lex"},
+                new Autor() { Id=2, Nombre="Alejandro"}
+            };
+        }
+    }
+}
